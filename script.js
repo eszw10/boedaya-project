542da00cd94f0b11ca9e97f715df8d21a1017be7
eryc_s.z.w <erycszw10@gmail.com>
2022-12-15 17:30:42 +0700

    add food and selling section with some fixes on features

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let navbar = document.getElementById("nav");
+let nava = document.getElementsByClassName("nava")
 window.onscroll = () => {
     "use strict";
     if(document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200) {
         navbar.classList.add("nav-scrolled");
+        for(let i=0; i<nava.length; i++) { 
+            nava[i].style.color='white';
+          }
     } else {
         navbar.classList.remove("nav-scrolled");
+        for(let i=0; i<nava.length; i++) { 
+            nava[i].style.color='black';
+          }
     }
 }
 
