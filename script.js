bbd472f99da4692e55cacd844767691d0e9811e0
eryc_s.z.w <erycszw10@gmail.com>
2022-12-17 15:15:23 +0700

    add user page and fix all of web section

--- script.js
+++ script.js
@@ -4,15 +4,12 @@ window.onscroll = () => {
     "use strict";
     if(document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200) {
         navbar.classList.add("nav-scrolled");
-        for(let i=0; i<nava.length; i++) { 
-            nava[i].style.color='white';
-          }
     } else {
         navbar.classList.remove("nav-scrolled");
-        for(let i=0; i<nava.length; i++) { 
-            nava[i].style.color='black';
-          }
     }
 }
 
+function peringatan() {
+    alert('Silahkan Sign Up atau Login terlebih dahulu!')
+}
 
